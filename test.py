01a8fc841ca41cd9a5f9dfed0c170a64faf30ec0
seanbenhur <seanbenhur@gmail.com>
2021-06-09 11:48:31 +0530

    added api

---
+++ test.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+# local url
+url = 'http://127.0.0.1:5000'
+
+#data
+data = {'cab_provider': 3
+      , 'source': 2
+      , 'destination': 1
+      , 'distance': 50,
+      'surge_multiplier': 1.0,
+      'cab_type': 3}
+
+data = json.dumps(data)
+
+send_request  = requests.post(url,data)
+print(send_request)
+print(send_request.json())
